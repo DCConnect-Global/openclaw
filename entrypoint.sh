7b9e53737c70de9ec25ca0e4fcc57cf927009c3c
Paul Desmond Parker <paul.parker@dcconnect.cn>
2026-03-17 15:13:51 +0800

    fix: remap docker compose invoking user to docker node

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-chown -R node:node /home/node/.openclaw
+
+# Remap node user to match host UID if OPENCLAW_UID is set
+if [ -n "$OPENCLAW_UID" ] && [ "$OPENCLAW_UID" != "$(id -u node)" ]; then
+  # The following automatically changes ownership in /home/node
+  usermod -u "$OPENCLAW_UID" node
+  groupmod -g "${OPENCLAW_GID:-$OPENCLAW_UID}" node
+  chown -R node:node /app/{extensions /app/skills}
+else
+  # In event that UID does not change, make sure of .openclaw ownership
+  chown -R node:node /home/node/.openclaw
+fi
+
 exec gosu node "$@"
